37470d21dffbfed5ae68881279566599c6ecb5a6
Daniel1249 <ionicadanielalin@yahoo.com>
2021-12-20 13:02:01 +0200

    data seed and data retriev for testing connection to DB

--- EFCore_Activity0302/EFCore_Activity0302/Program.cs
+++ EFCore_Activity0302/EFCore_Activity0302/Program.cs
@@ -1,6 +1,8 @@
 ﻿
 using EFCore_Activity0302;
 using EFCore_DBLibrary;
+using InventoryModels;
+//using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 
@@ -13,12 +15,49 @@ namespace EFCore_Activity0302
         static void Main(string[] args)
         {
             BuildOptions();
+            EnsureItems();
+            ListInventory();
         }
         static void BuildOptions()
         {
             _configuration = ConfigurationBuilderSingleton.ConfigurationRoot;
             _optionsBuilder = new DbContextOptionsBuilder<InventoryDbContext>();
             _optionsBuilder.UseSqlServer(_configuration.GetConnectionString("InventoryManager"));
+        }
+
+        static void EnsureItems()
+        {
+            EnsureItem("Batman Begins");
+            EnsureItem("Inception");
+            EnsureItem("Remember the Titans");
+            EnsureItem("Star Wars: The Empire Strikes Back");
+            EnsureItem("Top Gun");
+        }
+        private static void EnsureItem(string name)
+        {
+            using (var db = new InventoryDbContext(_optionsBuilder.Options))
+            {
+                //determine if item exists:
+                var existingItem = db.Items.FirstOrDefault(x => x.Name.ToLower()
+                == name.ToLower());
+
+                if (existingItem == null)
+                {
+                    //doesn't exist, add it.
+                    var item = new Item() { Name = name };
+                    db.Items.Add(item);
+                    db.SaveChanges();
+                }
+            }
+        }
+        private static void ListInventory()
+        {
+            using (var db = new InventoryDbContext(_optionsBuilder.Options))
+            {
+                var items = db.Items.OrderBy(x => x.Name).ToList();
+
+                items.ForEach(x => Console.WriteLine($"New Item: {x.Name}"));
+            }
         }
     }
 

19486532af7a2f4849f021c7107437fd6b353732
Daniel1249 <ionicadanielalin@yahoo.com>
2021-12-20 12:22:56 +0200

    added DBLibrary and Models Library and DB connection ad first migration

--- EFCore_Activity0302/EFCore_Activity0302/Program.cs
+++ EFCore_Activity0302/EFCore_Activity0302/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using EFCore_Activity0302;
+using EFCore_DBLibrary;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace EFCore_Activity0302
+{
+    public class Program
+    {
+        private static IConfigurationRoot _configuration;
+        private static DbContextOptionsBuilder<InventoryDbContext> _optionsBuilder;
+        static void Main(string[] args)
+        {
+            BuildOptions();
+        }
+        static void BuildOptions()
+        {
+            _configuration = ConfigurationBuilderSingleton.ConfigurationRoot;
+            _optionsBuilder = new DbContextOptionsBuilder<InventoryDbContext>();
+            _optionsBuilder.UseSqlServer(_configuration.GetConnectionString("InventoryManager"));
+        }
+    }
+
+}
---
+++ EFCore_Activity0302/EFCore_DBLibrary/InventoryDbContext.cs
@@ -0,0 +1,41 @@
+﻿using InventoryModels;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace EFCore_DBLibrary
+{
+    public class InventoryDbContext : DbContext
+    {
+       
+
+        private static IConfigurationRoot _configuration;
+
+        //Add a default constructor if scaffolding is needed
+        public InventoryDbContext()
+        {
+        }
+
+        //Add a complex constructor for allowing  Dependency Injection
+        public InventoryDbContext(DbContextOptions<InventoryDbContext> options)
+           : base(options)
+        {
+        }
+
+        public DbSet<Item> Items { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true,
+               reloadOnChange: true);
+                _configuration = builder.Build();
+                var cnstr = _configuration.GetConnectionString("InventoryManager");
+                optionsBuilder.UseSqlServer(cnstr);
+            }
+        }
+
+    }
+}

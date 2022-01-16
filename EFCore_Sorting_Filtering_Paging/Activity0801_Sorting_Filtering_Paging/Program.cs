4606446f335aa86b425d0943cd234cdee9564911
Daniel1249 <ionicadanielalin@yahoo.com>
2022-01-17 00:09:49 +0200

    filtering, sorting, paging

---
+++ EFCore_Sorting_Filtering_Paging/Activity0801_Sorting_Filtering_Paging/Program.cs
@@ -0,0 +1,54 @@
+﻿using EF_Activity001;
+using InventoryHelpers;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Linq;
+
+namespace Activity0801_Sorting_Filtering_Paging
+{
+    class Program
+    {
+        private static IConfigurationRoot _configuration;
+        private static DbContextOptionsBuilder<AdventureWorksContext> _optionsBuilder;
+
+        static void Main(string[] args)
+        {
+            BuildOptions();
+            ListPeopleThenOrderAndTake();
+            QueryPeopleOrderedToListAndTake();
+
+        }
+
+        static void BuildOptions()
+        {
+            _configuration = ConfigurationBuilderSingleton.ConfigurationRoot;
+            _optionsBuilder = new DbContextOptionsBuilder<AdventureWorksContext>();
+            _optionsBuilder.UseSqlServer(_configuration.GetConnectionString("AdventureWorks"));
+        }
+
+        private static void ListPeopleThenOrderAndTake()
+        {
+            using (var db = new AdventureWorksContext(_optionsBuilder.Options))
+            {
+                var people = db.Person.ToList().OrderByDescending(x => x.LastName);
+                foreach (var person in people.Take(10))
+                {
+                    Console.WriteLine($"{person.FirstName} {person.LastName}");
+                }
+            }
+        }
+        private static void QueryPeopleOrderedToListAndTake()
+        {
+            using (var db = new AdventureWorksContext(_optionsBuilder.Options))
+            {
+                var query = db.Person.OrderByDescending(x => x.LastName);
+                var result = query.Take(10);
+                foreach (var person in result)
+                {
+                    Console.WriteLine($"{person.FirstName} {person.LastName}");
+                }
+            }
+        }
+    }
+}

418664a340f23919d563049feeb483305c94fb1c
Daniel1249 <ionicadanielalin@yahoo.com>
2022-02-16 12:34:57 +0200

    layeringOurSolution with AutoMapper

---
+++ AutoMapper/InventoryModels/Dtos/CategoryDetailDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventoryModels.Dtos
+{
+    public class CategoryDetailDto
+    {
+        public string Color { get; set; }
+        public string Value { get; set; }
+    }
+}
---
+++ AutoMapper/InventoryModels/Dtos/CategoryDto.cs
@@ -0,0 +1,10 @@
+﻿namespace InventoryModels.Dtos
+{
+    public class CategoryDto
+    {
+        public int Id { get; set; }
+        public string Category { get; set; }
+        public CategoryColorDto CategoryColor { get; set; }
+
+    }
+}
